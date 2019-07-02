241371354c66b6615cde81db94cecc76c4708ba0
Dromich <hrushch.rr57@gmail.com>
2019-07-02 14:56:22 +0300

    send photo Markdown

--- main.js
+++ main.js
@@ -30,35 +30,61 @@ function random(min, max) {
 
 bot.on('message',msg =>{
 
-	const { id } = msg.chat 
-	const name = msg.chat.first_name
-	
-	pexelsClient.getPhoto(random(1111111,1112222))
-    .then(function(result){
-		//console.log(result);
 
-		if (result.status == '404'  ) {
+const { id } = msg.chat 
+const name = msg.chat.first_name
+
+
+	
+	// pexelsClient.getPhoto(random(1111111,1112222))
+    // .then(function(result){
+	// 	//console.log(result);
+
+	// 	if (result.status == '404'  ) {
 		
-			bot.sendMessage(id,'Помилка ' + result.error )
+	// 		bot.sendMessage(id,'Помилка ' + result.error )
 
-		}else{
-			bot.sendPhoto(id, result.src.large2x,{
-				caption:`Фотграф ${result.photographer} `
-			})
+	// 	}else{
+	// 		bot.sendPhoto(id, result.src.large2x,{
+	// 			caption:`Фотграф ${result.photographer} `
+	// 		})
 	
-			bot.sendMessage(id,'фото на сайті ' + result.url )
+	// 		bot.sendMessage(id,'фото на сайті ' + result.url )
 
+			
+	// 	}
+
+		
+    // }).
+    // catch(function(e){
+	// 	bot.sendMessage(id,'Помилка ' + e); 
+
+    //     console.log('помилка ' + e);
+	// });
+	
+
+
+	pexelsClient.search("cars", 10, 1)
+    .then(function(result){
+		console.log(result);
+		
+		for (let index = 0; index < result.photos.length; index++) {
+			const element = result.photos[index];
+
+			bot.sendPhoto(id, element.src.large,{
+							caption:`Фотограф  [${element.photographer}](${element.photographer_url}).  `,
+							parse_mode:'Markdown'
+						})
+				
+						//bot.sendMessage(id,'фото на сайті ' + element.url )
 			
 		}
 
-		
-    }).
-    catch(function(e){
-		bot.sendMessage(id,'Помилка ' + e); 
 
-        console.log('помилка ' + e);
-	});
-	
+
+    }).catch(function(e){
+        console.err(e);
+    });
 	
 
 })
